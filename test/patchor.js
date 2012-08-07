04909b74e51de7e891c0c8b84695be86a1a13ee1
pjaspers <junkiesxl@gmail.com>
2012-08-07 18:13:11 +0200

    Global ignore file ignored needed file, teh suck.

---
+++ test/patchor.js
@@ -0,0 +1,16 @@
+/*
+ As of version 1.1.2, Propane will load and execute the contents of
+ ~Library/Application Support/Propane/unsupported/caveatPatchor.js
+ immediately following the execution of its own enhancer.js file.
+
+ You can use this mechanism to add your own customizations to Campfire
+ in Propane.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
